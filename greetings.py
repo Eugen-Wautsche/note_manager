99104aae2015d8b77aa0b173e17b0b48c5d719a2
Eugen-Wautsche <ugene_wd1@bk.ru>
2025-01-14 23:41:51 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,9 @@
+
+username = Eugen
+title = Title
+content = Description
+status = Status
+created_date = DateTime
+#в формате "день-месяц-год", например "10-11-2024"
+issue_date = Deadline
+#(дедлайн) в формате "день-месяц-год", например "10-12-2024"
