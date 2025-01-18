31675aa9fa89c0c45fbb4e1e22612c79d73b8910
Eugen-Wautsche <ugene_wd1@bk.ru>
2025-01-18 16:43:32 +0300

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -1,9 +1,11 @@
 
-username = Eugen
-title = Title
-content = Description
-status = Status
-created_date = DateTime
-#в формате "день-месяц-год", например "10-11-2024"
-issue_date = Deadline
+username = 'Имя'
+title = 'Заголовок заметки'
+content = 'Описание заметки'
+status = 'Статус заметки'
+created_date = 'Дата заметки'
+#Дата заметки в формате (день.месяц.год),например 10.11.2024"
+issue_date = 'Дедлайн'
 #(дедлайн) в формате "день-месяц-год", например "10-12-2024"
+print(username, title, content, status, created_date, issue_date, sep='\n')
+
